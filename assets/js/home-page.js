cce72bf13a25437d3b42da6bafac2624627ade9a
mendoza000 <omarmendoza.alv25@gmail.com>
2021-09-07 16:04:29 -0400

    add nav responsive

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,5 +1,6 @@
 const app = document.querySelector('#app');
-
+const btnMenu = document.querySelector('.btn_menu');
+const navMenu = document.querySelector('.app_nav');
 
 export const writeHomePage = () => {
 
@@ -10,5 +11,31 @@ export const writeHomePage = () => {
   		
 	`*/
 
+	eventsHomePage()
+
+
+}
+
+const eventsHomePage = () => {
+
+	let view = false
+
+	btnMenu.addEventListener('click', (e) => {
+		
+		if (!view) {
+			navMenu.style.left = "0%"
+			btnMenu.innerHTML = `
+				<i class="las la-times"></i>
+			`
+			view = !view
+		}else{
+			navMenu.style.left = "-100%"
+			btnMenu.innerHTML = `
+				<i class="las la-bars"></i>
+			`
+			view = !view
+		}
+
+	});
 
 }
