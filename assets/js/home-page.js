59a447ec791a1a0bafd553f2e4cffdbbee4a6591
mendoza000 <omarmendoza.alv25@gmail.com>
2021-09-07 13:51:04 -0400

    primer commit con el header

---
+++ assets/js/home-page.js
@@ -0,0 +1,14 @@
+const app = document.querySelector('#app');
+
+
+export const writeHomePage = () => {
+
+	console.log("Hello world");
+
+	/*document.querySelector('#app').innerHTML = `
+  		<h1 class="app_title">Garua Token!</h1>
+  		
+	`*/
+
+
+}
